6334795827af804b5df583fb9bddb717053757ef
Jiahui Liu <jiahuiliu@wireless-10-148-68-217.public.utexas.edu>
2022-05-09 01:59:59 -0500

    clean up

--- cpp-lwevss-main-MODIFIED/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/jiahuiliu/Dropbox/UTcourses/Spring2022/distributed/project-related/cpp-lwevss-main-MODIFIED
-# Build directory: /Users/jiahuiliu/Dropbox/UTcourses/Spring2022/distributed/project-related/cpp-lwevss-main-MODIFIED
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests")
---
+++ cpp-lwevss-main/tests/CMakeFiles/test_merlin.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/jiahuiliu/Dropbox/PVSS/380D/cpp-lwevss-main/tests/test_merlin.cpp" "tests/CMakeFiles/test_merlin.dir/test_merlin.cpp.o" "gcc" "tests/CMakeFiles/test_merlin.dir/test_merlin.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/jiahuiliu/Dropbox/PVSS/380D/cpp-lwevss-main/CMakeFiles/lwe-pvss.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
